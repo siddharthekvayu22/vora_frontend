3e3f2a192c18493278175150334abaa7a79066b7
Saksham Kamboj <saksham@ekvayu.com>
2026-02-03 15:19:47 +0530

    remove duplicate icons

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -49,13 +49,10 @@ import {
   MdArrowDownward,
   MdArrowBack,
   MdArrowForward,
-  MdKeyboardArrowDown,
-  MdKeyboardArrowLeft,
 
   // Time & Calendar
   MdAccessTime,
   MdCalendarToday,
-  MdSchedule,
 
   // Communication
   MdEmail,
@@ -79,8 +76,6 @@ import {
   // Charts & Reports
   MdBarChart,
   MdShowChart,
-  MdAssessment,
-  MdReport,
 
   // Theme
   MdLightMode,
@@ -99,7 +94,7 @@ import {
 } from "react-icons/md";
 
 import {
-  // Additional icons from other icon sets
+  // Additional icons from Feather Icons
   FiActivity,
   FiClock,
   FiUser,
@@ -119,62 +114,44 @@ import {
 const iconMap = {
   // Navigation icons
   dashboard: MdDashboard,
-  "icon-dashboard": MdDashboard,
   home: MdHome,
-  "icon-home": MdHome,
   menu: MdMenu,
-  "icon-menu": MdMenu,
 
   // Document icons
   document: MdDescription,
-  "icon-document": MdDescription,
   file: MdInsertDriveFile,
-  "icon-file": MdInsertDriveFile,
   docs: HiOutlineDocumentText,
 
   // Upload/Download icons
   upload: MdUpload,
-  "icon-upload": MdUpload,
   download: MdDownload,
-  "icon-download": MdDownload,
 
   // Search/Analysis icons
   search: MdSearch,
-  "icon-search": MdSearch,
   analyze: MdAnalytics,
   analytics: MdShowChart,
 
   // Security & Framework icons
   shield: MdShield,
-  "icon-shield": MdShield,
   "shield-check": MdVerified,
   security: MdSecurity,
-  "icon-security": MdSecurity,
   lock: MdLock,
-  "icon-lock": MdLock,
   eye: MdVisibility,
-  "icon-eye": MdVisibility,
   "eye-off": MdVisibilityOff,
-  "icon-eye-off": MdVisibilityOff,
 
   // Chart/Analytics icons
   chart: MdBarChart,
-  "icon-chart": MdBarChart,
 
   // Settings/Config icons
   settings: MdSettings,
-  "icon-settings": MdSettings,
   gear: MdBuild,
 
   // Book/Documentation icons
   book: MdBook,
-  "icon-book": MdBook,
 
   // User/Profile icons
   user: FiUser,
-  "icon-user": FiUser,
   users: FiUsers,
-  "icon-users": FiUsers,
   profile: MdAccountCircle,
   "user-plus": FiUserPlus,
   "user-minus": FiUserMinus,
@@ -182,117 +159,75 @@ const iconMap = {
 
   // Status icons
   check: MdCheck,
-  "icon-check": MdCheck,
   checkmark: MdCheck,
   success: MdCheckCircle,
   "check-circle": MdCheckCircle,
   warning: MdWarning,
-  "icon-warning": MdWarning,
   error: MdError,
-  "icon-error": MdError,
   info: MdInfo,
-  "icon-info": MdInfo,
   "x-circle": MdCancel,
 
   // Arrow icons
   "arrow-up": MdArrowUpward,
-  "icon-arrow-up": MdArrowUpward,
   "arrow-down": MdArrowDownward,
-  "icon-arrow-down": MdArrowDownward,
   "arrow-left": MdArrowBack,
-  "icon-arrow-left": MdArrowBack,
   "arrow-right": MdArrowForward,
-  "icon-arrow-right": MdArrowForward,
 
   // Action icons
   plus: MdAdd,
-  "icon-plus": MdAdd,
   add: MdAdd,
   minus: MdRemove,
-  "icon-minus": MdRemove,
   close: MdClose,
-  "icon-close": MdClose,
   x: MdClose,
-  "icon-x": MdClose,
   delete: MdDelete,
-  "icon-delete": MdDelete,
   trash: MdDelete,
-  "icon-trash": MdDelete,
   edit: MdEdit,
-  "icon-edit": MdEdit,
 
   // Time icons
   clock: FiClock,
-  "icon-clock": FiClock,
   time: MdAccessTime,
   calendar: MdCalendarToday,
-  "icon-calendar": MdCalendarToday,
 
   // Folder icons
   folder: MdFolder,
-  "icon-folder": MdFolder,
   "folder-open": MdFolderOpen,
 
   // List icons
   list: MdList,
-  "icon-list": MdList,
 
   // Communication icons
   mail: MdEmail,
-  "icon-mail": MdEmail,
   email: MdEmail,
-  "icon-email": MdEmail,
   phone: MdPhone,
-  "icon-phone": MdPhone,
   message: MdMessage,
   notification: MdNotifications,
-  "icon-notification": MdNotifications,
 
   // Theme icons
   sun: MdLightMode,
-  "icon-sun": MdLightMode,
   moon: MdDarkMode,
-  "icon-moon": MdDarkMode,
 
   // Misc icons
   star: MdStar,
-  "icon-star": MdStar,
   heart: MdFavorite,
-  "icon-heart": MdFavorite,
   refresh: MdRefresh,
-  "icon-refresh": MdRefresh,
   link: MdLink,
-  "icon-link": MdLink,
   tag: MdLabel,
-  "icon-tag": MdLabel,
   lightbulb: MdLightbulb,
-  "icon-lightbulb": MdLightbulb,
   power: MdPowerSettingsNew,
-  "icon-power": MdPowerSettingsNew,
 
   // Compliance/Audit specific
   audit: MdSearch,
-  "icon-audit": MdSearch,
   compliance: MdVerifiedUser,
-  "icon-compliance": MdVerifiedUser,
   framework: MdWork,
-  "icon-framework": MdWork,
   report: HiOutlineClipboardList,
-  "icon-report": HiOutlineClipboardList,
 
   // File type icons
   pdf: MdPictureAsPdf,
-  "icon-pdf": MdPictureAsPdf,
   doc: MdDescription,
-  "icon-doc": MdDescription,
   excel: MdTableChart,
-  "icon-excel": MdTableChart,
   ppt: MdDescription,
-  "icon-ppt": MdDescription,
   csv: MdTableChart,
-  "icon-csv": MdTableChart,
   zip: MdFolder,
-  "icon-zip": MdFolder,
 
   // Activity specific
   activity: FiActivity,
@@ -305,7 +240,7 @@ export default function Icon({
   className = "",
 }) {
   // Get the icon component from the map
-  const IconComponent = iconMap[name] || iconMap[`icon-${name}`] || MdInfo;
+  const IconComponent = iconMap[name] || MdInfo;
 
   // Convert size to number if it's a string with 'px'
   const iconSize =
